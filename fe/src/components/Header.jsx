36dae638fe3201bc1be98678bd2288077b84b513
Lê Nhật Sơn <nhatsonle2309@gmail.com>
2025-04-02 10:56:43 +0700

    Update the header

--- fe/src/components/Header.jsx
+++ fe/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import Logo from '../assets/Logo.svg'
 import Notification from '../assets/Vector.svg'
 import Avatar from '../assets/Avatar.svg'
+import Search from '../assets/Search.svg'
 function Header(){
   return (
     <div className="fixed flex items-center justify-between h-16 px-8 bg-white top-0 left-0 right-0 z-50 shadow-md">
@@ -16,20 +17,7 @@ function Header(){
           placeholder="Search for rooms and offers"
           className="w-full h-10 pl-10 pr-4 text-gray-600 bg-gray-100 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M21 21l-4.35-4.35m1.85-5.15a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
+        <img src={Search} className='absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" '/>
       </div>
 
 
