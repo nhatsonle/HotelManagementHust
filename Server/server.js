53634e4d9fa1db5a6a2c5f3ec60b23b498a28132
Khang <chuvinhkhang.chukid@gmail.com>
2025-05-12 14:58:45 +0700

    api routes mount

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
+const apiRouter = require('./src/api/index');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// Mount the API router
+app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
   res.send('Hotel Management API is running!');
 });
 
-// TODO: Mount routers from ./src/api when ready
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    error: 'Something went wrong!',
+    message: err.message
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- Server/src/api/index.js
+++ Server/src/api/index.js
@@ -1,16 +1,12 @@
 // Định nghĩa các API endpoints cho từng resource (room, guest, booking, ...)
 // Mount các router con (vd: /api/v1)
-
 const express = require('express');
-const roomRouter = require('./room');
-const guestRouter = require('./guest');
-const bookingRouter = require('./booking');
+const roomRouter = require('./room.routes');
 
 const apiRouter = express.Router();
 
 // Mount các router con
 apiRouter.use('/rooms', roomRouter);
-apiRouter.use('/guests', guestRouter);
-apiRouter.use('/bookings', bookingRouter);
+
 
 module.exports = apiRouter;
