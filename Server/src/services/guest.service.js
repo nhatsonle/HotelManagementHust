dd266711ee4d6187bae45645582dbd3e2d5b8c7b
Oc <banhmythom2@gmail.com>
2025-05-24 22:09:41 +0700

    post guest update

--- Server/src/services/guest.service.js
+++ Server/src/services/guest.service.js
@@ -5,13 +5,16 @@ exports.getAllGuests = () => guestQueries.getAllGuests();
 exports.getGuestById = (guest_id) => guestQueries.getGuestById(guest_id);
 
 exports.createGuest = (guest) => {
-  const { name, phone, email } = guest;
-  return guestQueries.createGuest(name, phone, email);
+  const { name, phone, email, passport_number, city, region, address, zip_code } = guest;
+  if (!name || !phone || !email) {
+    throw new Error('Tên, số điện thoại và email là bắt buộc');
+  }
+  return guestQueries.createGuest(name, phone, email, passport_number, city, region, address, zip_code);
 };
 
 exports.updateGuest = (guest_id, guest) => {
-  const { name, phone, email } = guest;
-  return guestQueries.updateGuest(guest_id, name, phone, email);
+  const { name, phone, email, passport_number, city, region, address, zip_code } = guest;
+  return guestQueries.updateGuest(guest_id, name, phone, email, passport_number, city, region, address, zip_code);
 };
 
 exports.deleteGuest = (guest_id) => guestQueries.deleteGuest(guest_id);
